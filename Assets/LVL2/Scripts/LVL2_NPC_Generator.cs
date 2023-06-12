a61dbcf8b3deddb09da0d13cb803378c25e86470
twp3964 <twp3964@g.rit.edu>
2023-06-12 11:31:54 -0400

    Please Resolve merge conflict

--- Assets/LVL2/Scripts/LVL2_NPC_Generator.cs
+++ Assets/LVL2/Scripts/LVL2_NPC_Generator.cs
@@ -16,20 +16,21 @@ public class LVL2_NPC_Generator : MonoBehaviour
     {
 
         Collider[] childColliders = GetComponentsInChildren<Collider>();
-        for (int i = 0; i < npcCount; i++){
+        for (int i = 0; i < npcCount; i++)
+        {
 
             int randomArea = Random.Range(0, childColliders.Length);
             Bounds childBound = childColliders[randomArea].bounds;
             Vector3 randomPosition = GetRandomPositionWithinBounds(childBound);
             Instantiate(npcPrefab, randomPosition, Quaternion.identity);
-            }
-     }
+        }
+    }
 
     private Vector3 GetRandomPositionWithinBounds(Bounds bounds)
     {
         Vector3 randomPosition = Vector3.zero;
+
         randomPosition = new Vector3(
-            
             Random.Range(bounds.min.x, bounds.max.x),
             10.6f, // Set Y-position to ground level
             Random.Range(bounds.min.z, bounds.max.z)
