cdb2e980b9bfea2820558bf9e3966ef24a7c08c9
William Tallarico <wct9667@rit.edu>
2023-06-13 10:01:01 -0400

    more minor scene changes

--- Assets/LVL2/Scripts/LVL2_FPSCounter.cs
+++ Assets/LVL2/Scripts/LVL2_FPSCounter.cs
@@ -2,35 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class LVL2_FPSCounter : MonoBehaviour
 {
     //text to be modified to display the fps
     
     private TextMeshProUGUI fpsCounterText;
-    private float pollingTime = 1f; //how often the fps will update
-    private float time;
-    private int frameCount;
+
+    public float updateInterval = 0.5f; //How often should the number update
+
+    float accum = 0.0f;
+    int frames = 0;
+    float timeleft;
+    float fps;
 
 
-    private void Start()
+    // Use this for initialization
+    void Start()
     {
+        timeleft = updateInterval;
         fpsCounterText = this.gameObject.GetComponent<TextMeshProUGUI>();
     }
+
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
 
-        frameCount++;
-
-        if (time >= pollingTime)
+        // Interval ended - update GUI text and start new interval
+        if (timeleft <= 0.0)
         {
-            int frameRate = Mathf.RoundToInt(frameCount / time);
-            fpsCounterText.text = frameRate.ToString() +" FPS";
-
-            time -= pollingTime;
-            frameCount = 0;
+            // display two fractional digits (f2 format)
+            fps = Mathf.RoundToInt(accum / frames);
+            fpsCounterText.text = fps.ToString() + " FPS";
+            timeleft = updateInterval;
+            accum = 0.0f;
+            frames = 0;
         }
     }
 }
+
