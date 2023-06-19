238780459b203c0db3f0df7526ba22e9e1658294
twp3964 <twp3964@g.rit.edu>
2023-06-19 10:02:16 -0400

    They can move but just a lil funky
    Check out lv2 Script test for funny wacky movement

--- Assets/LVL2/Scripts/LVL2_NPC_Wander.cs
+++ Assets/LVL2/Scripts/LVL2_NPC_Wander.cs
@@ -4,35 +4,62 @@ using UnityEngine;
 
 public class LVL2_NPC_Wander : MonoBehaviour
 {
-    public float movementSpeed = 3f;
+    public float movementSpeed = 2f;
+    public float stoppingDistance = 1f; // Distance to stop before the waypoint
 
     public Transform[] waypoints;
     public int currentLocation = 0;
     private Transform currentWaypoint;
-    public float waypointReachedThreshold = 0.1f; // Distance threshold to consider a waypoint reached
 
+    private bool isMoving = true;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetNextWaypoint();
+    }
 
     private void Update()
     {
+        if (!isMoving)
+        {
+            return;
+        }
+
         if (waypoints.Length == 0)
         {
-            Debug.LogWarning("No waypoints assigned to NPCController on " + gameObject.name);
+            Debug.LogWarning("No waypoints assigned to LVL2_NPC_Wander on " + gameObject.name);
             return;
         }
 
         // Move towards the current waypoint
-        Transform currentWaypoint = waypoints[currentLocation];
-        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, movementSpeed * Time.deltaTime);
+        Vector3 direction = (currentWaypoint.position - transform.position).normalized;
+        direction.y = 0;
+        rb.AddForce(direction * movementSpeed * Time.deltaTime, ForceMode.VelocityChange);
 
         // Rotate towards the current waypoint
-        Quaternion targetRotation = Quaternion.LookRotation(currentWaypoint.position - transform.position);
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
 
         // Check if reached the current waypoint
-        if (Vector3.Distance(transform.position, currentWaypoint.position) < waypointReachedThreshold)
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < stoppingDistance)
         {
-            // Move to the next waypoint
-            currentLocation = (currentLocation + 1) % waypoints.Length;
+            SetNextWaypoint();
         }
+    }
+
+    private void SetNextWaypoint()
+    {
+        currentLocation = (currentLocation + 1) % waypoints.Length;
+        currentWaypoint = waypoints[currentLocation];
+        StartCoroutine(StopAtWaypoint());
+    }
+
+    private IEnumerator StopAtWaypoint()
+    {
+        isMoving = false;
+        yield return new WaitForSeconds(Random.Range(1f, 3f));
+        isMoving = true;
     }
 }
