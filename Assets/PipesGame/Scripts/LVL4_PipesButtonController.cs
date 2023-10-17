dcc78ad89ad2d31f77b9ff8200290810c09d90dd
LorenzoM <Lorenzo.montanaro01@gmail.com>
2023-10-17 11:38:01 +0200

    Pipes Game Update. CheckIfTheGameISFinished and 1 game pattern added

--- Assets/PipesGame/Scripts/LVL4_PipesButtonController.cs
+++ Assets/PipesGame/Scripts/LVL4_PipesButtonController.cs
@@ -37,8 +37,44 @@ public class LVL4_PipesButtonController : MonoBehaviour
     }
     public void OnClick()
     {
-        Debug.Log("premuto");
-        pivot.Rotate(0, 0, -90);
-        //this.transform.parent.Rotate(0, 0, 90);
+        switch (PipeButtonType)
+        {
+            case EPipeButtonType.Angled:
+                {
+                    if (pivot.transform.eulerAngles.z >= 270)
+                    {
+                        //Debug.Log("if");
+                        //Debug.Log(pivot.transform.eulerAngles.z);
+                        pivot.Rotate(0, 0, -270);
+                        //Debug.Log(pivot.transform.eulerAngles.z);
+                    }
+                    else
+                    {
+                        //Debug.Log("else");
+                        //Debug.Log(pivot.transform.eulerAngles.z);
+                        pivot.Rotate(0, 0, 90);
+                        //Debug.Log(pivot.transform.eulerAngles.z);
+                    }
+                }
+                break;
+            case EPipeButtonType.Straight:
+                {
+                    if (pivot.transform.eulerAngles.z >= 90)
+                    {
+                        pivot.Rotate(0, 0, -pivot.transform.eulerAngles.z);
+                    }
+                    else
+                    {
+                        pivot.Rotate(0, 0, 90);
+                    }
+                }
+                break;
+            default:
+                {
+                    Debug.Log("Case Default");
+                }
+                break;
+        }
+
     }
 }
--- Assets/PipesGame/Scripts/LVL4_PipesGameController.cs
+++ Assets/PipesGame/Scripts/LVL4_PipesGameController.cs
@@ -12,7 +12,7 @@ public class LVL4_PipesGameController : MonoBehaviour
     [SerializeField]
     private Transform gameGrid;
     [SerializeField]
-    private List<LVL4_PipesButtonController> buttons = new();
+    public List<LVL4_PipesButtonController> buttons = new();
     
     // Start is called before the first frame update
     void Start()
@@ -23,13 +23,15 @@ public class LVL4_PipesGameController : MonoBehaviour
 
     void AddButtons()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 16; i++)
         {
             GameObject button = Instantiate(gamePatternsSO[0].Pattern[i].button, gameGrid);
             //button.transform.SetParent(gameGrid, false);
 
             buttons.Add(button.GetComponent<LVL4_PipesButtonController>());
-            //add the sprite of the cardCover
+            //buttons[i].Pivot.Rotate(0, 0, 0);
+            
+            
             switch (buttons[i].PipeButtonType)
             {
                 case EPipeButtonType.Angled:
@@ -50,15 +52,35 @@ public class LVL4_PipesGameController : MonoBehaviour
                     }
                     break;
             }
+            
             //Debug.Log((float)gamePatternsSO[0].Pattern[i].rotation);
             //Debug.Log(i);
             //Debug.Break();
             //buttons[i].Pivot.Rotate(0, 0, 90);
         }
     }
-    // Update is called once per frame
-    void Update()
+
+    public void CheckIfTheGameISFinished()
     {
-        
+        int i = 0;
+        foreach (LVL4_PipesButtonController button in buttons)
+        {
+           
+            if (button.Pivot.transform.eulerAngles.z == (int)gamePatternsSO[0].Pattern[i].rotation * 90) 
+            {
+                //Debug.Log(button);
+                //Debug.Log("ok");
+            }
+            else
+            {
+                Debug.LogError(button);
+                Debug.LogError(button.Pivot.transform.eulerAngles.z);
+                Debug.LogError((int)gamePatternsSO[0].Pattern[i].rotation * 90);
+                Debug.LogError(i);
+                break;
+            }
+            i++;
+        }
+        Debug.Log("ok");
     }
 }
