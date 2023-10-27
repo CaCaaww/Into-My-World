b74e99af57b46ddd5466b1b784eb7dfce5416534
LorenzoM <Lorenzo.montanaro01@gmail.com>
2023-10-27 18:09:55 +0200

    MathWarGame Update

--- Assets/MathWarsGame/Scripts/MathWarGameController.cs
+++ Assets/MathWarsGame/Scripts/MathWarGameController.cs
@@ -2,51 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 public class MathWarGameController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject player;
+    private List<LVL4_MathWarGameFloorSO> gameFloorsSO;
     [SerializeField]
-    private GameObject enemy;
+    private Transform gameGrid;
+    //[SerializeField]
+    //private int levelIndex;
     [SerializeField]
-    private TextMeshProUGUI playerScoreRef;
-    [SerializeField]
-    private TextMeshProUGUI enemyScoreRef;
+    private GameObject floor;
+
+    public string enemyText;
     // Start is called before the first frame update
     void Start()
     {
-        //playerStartPos = player.GetComponent<RectTransform>();
+        enemyText = floor.GetComponentInChildren<TextMeshProUGUI>().ToString();
+
+        AddFloors();
     }
 
     // Update is called once per frame
-    void Update()
+    void AddFloors()
     {
-        
-    }
-    public void whenClicked()
-    {
-
-        Debug.Log("click");
-        //playerScore.text = "risultato";
-        int playerScore = int.Parse(playerScoreRef.text);
-        int enemyScore = int.Parse(enemyScoreRef.text);
-        if (playerScore > enemyScore)
+        for (int i = 0; i < 3; i++)
         {
-            //Debug.Log("it works");
-            player.transform.position = transform.position + Vector3.down * 70f;
-            this.gameObject.SetActive(false);
-            playerScore += enemyScore;
-            enemy.SetActive(false);
-            Debug.Log(playerScore);
-            playerScoreRef.text = playerScore.ToString();
-        }
-        else
-        {
-            //Debug.Log(playerStartPos.transform.position);
-            player.transform.position = new Vector3(500, 300, 0);
-
+            GameObject button = Instantiate(floor, gameGrid);
+            enemyText = "test";
         }
     }
 }
