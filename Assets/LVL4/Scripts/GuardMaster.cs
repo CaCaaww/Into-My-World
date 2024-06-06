d7ebf7f93937d59eea8f0635481fd77075e4e9d9
JoshClark7 <joshtclark7@gmail.com>
2024-06-06 15:40:16 +0200

    Added some scripts

---
+++ Assets/LVL4/Scripts/GuardMaster.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardMaster : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
