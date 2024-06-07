895009917836078ae5d4943b5ae6a3679041bfdf
Eleanor Rose Tallman <eleanortallman.business@gmail.com>
2024-06-07 17:53:42 +0200

    Overall Work on UI Manager

--- Assets/LVL4/Scripts/LVL4Manager.cs
+++ Assets/LVL4/Scripts/LVL4Manager.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LVL4Manager : MonoBehaviour
 {
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadSceneAsync("LVL4");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 }
