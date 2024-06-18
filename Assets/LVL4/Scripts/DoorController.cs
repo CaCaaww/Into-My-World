5c560780600cc15db4f16988fe6ee4d12c739950
Ryan Lembo-Ehms <rlemboehms@gmail.com>
2024-06-18 19:53:05 +0200

    Document, remove magic numbers

--- Assets/LVL4/Scripts/DoorController.cs
+++ Assets/LVL4/Scripts/DoorController.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
+    #region Inspector
     [SerializeField] private GameObject leftDoor;
     [SerializeField] private GameObject rightDoor;
     [SerializeField] private bool onLeftSideOfHall;
-    //[SerializeField] private GameObject lockObject;
-    //[SerializeField] private RectTransform pivot;
+    #endregion
+
+    #region Private Variables
+    private const float xRotation = 0;
+    private const float yRotationLeft = 0;
+    private const float yRotationRight = -180;
+    private const float zRotation = 0;
+    #endregion
+
     public void toggleDoor()
     {
-        //Debug.Log("Toggling doors");
+        // Without checking what side of the hall the door is on, then
+        // all doors would open in the same direction globally. Doors should
+        // open in the same direction relative to the player
         if (onLeftSideOfHall)
         {
-            leftDoor.transform.rotation = Quaternion.Euler(0, 0, 0);
-            rightDoor.transform.rotation = Quaternion.Euler(0, -180, 0);
+            leftDoor.transform.rotation = Quaternion.Euler(xRotation, yRotationLeft, zRotation);
+            rightDoor.transform.rotation = Quaternion.Euler(xRotation, yRotationRight, zRotation);
         }
         else
         {
-            leftDoor.transform.rotation = Quaternion.Euler(0, -180, 0);
-            rightDoor.transform.rotation = Quaternion.Euler(0, 0, 0);
+            leftDoor.transform.rotation = Quaternion.Euler(xRotation, yRotationRight, zRotation);
+            rightDoor.transform.rotation = Quaternion.Euler(xRotation, yRotationLeft, zRotation);
         }
     }
 }
--- Assets/LVL4/Scripts/Quit.cs
+++ Assets/LVL4/Scripts/Quit.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class Quit : MonoBehaviour
 {
 
+    [Tooltip("The Red X in the top left corner")]
     [SerializeField]
     private GameObject RedButton;
+    [Tooltip("Confirmation page")]
     [SerializeField]
     private GameObject AreYouSure;
+    [Tooltip("Yes button")]
     [SerializeField]
     private GameObject YesButton;
+    [Tooltip("No button")]
     [SerializeField]
     private GameObject NoButton;
+    [Tooltip("The dimmed screen")]
     [SerializeField]
     private GameObject Backdrop;
+    [Tooltip("Canvas the game is played on")]
     [SerializeField]
     private Canvas gameCanvas;
 
     public void RedXButtonClicked()
     {
+        // Opening the quit screen
         AreYouSure.SetActive(!AreYouSure.activeSelf);
         YesButton.SetActive(!YesButton.activeSelf);
         NoButton.SetActive(!NoButton.activeSelf);
@@ -28,22 +35,29 @@ public class Quit : MonoBehaviour
 
     public void YesButtonClicked()
     {
-        // Sets the game canvas to inactive, the player will return to the level
         AreYouSure.SetActive(false);
         YesButton.SetActive(false);
         NoButton.SetActive(false);
         Backdrop.SetActive(false);
-        //this.gameObject.transform.parent.gameObject.transform.parent.GetComponent<Canvas>().enabled = false;
+        
+        // Using .enable and .disable instead of gameObject.SetActive because
+        // of issues with reactiviting children when the parent gameObject is
+        // activated
         if (gameCanvas != null) {
             gameCanvas.enabled = false;
         }
+
+        // Enable
         LVL4Manager.instance.TogglePlayerInput();
+
+        // Returning to the level, hide the cursor
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void NoButtonClicked()
     {
+        // Close the quit screen and continue playing the minigame
         AreYouSure.SetActive(false);
         YesButton.SetActive(false);
         NoButton.SetActive(false);
