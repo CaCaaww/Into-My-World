6f3b4854743dba435ee7e78c4c736f35cad96cc8
BirdTheProgrammer <JamesAllenHansplant@gmail.com>
2024-06-28 08:34:42 -0400

    James
    Simple UI and unimplemented event for moving to next stage 6-28-24

---
+++ Assets/LVL4/Scripts/Events/AllPrisonersFreedEventChannel.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "All Prisoners Freed Event Channel", menuName = "Events/All Prisoners Freed Event Channel")]
+public class AllPrisonersFreedEventChannel : GenericEventChannelSO<AllPrisonersFreedEvent> {
+
+}
+[System.Serializable]
+public struct AllPrisonersFreedEvent { 
+
+}
+
