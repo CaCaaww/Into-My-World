68a1674a1009ba955da8a8dab5e775a5dd2a8b05
Eleanor Rose Tallman <eleanortallman.business@gmail.com>
2024-07-01 16:46:08 +0200

    Reapply "merge"
    This reverts commit 0c30b33696e6688e175b598980e416b44133e1c9.

---
+++ Assets/LVL4/Scripts/Events/OpenNextStageEventChannel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Open Next Stage Event Channel", menuName = "Events/Open Next Stage Event Channel")]
+public class OpenNextStageEventChannel : GenericEventChannelSO<OpenNextStageEvent>
+{
+
+}
+[System.Serializable]
+public struct OpenNextStageEvent {
+
+}
