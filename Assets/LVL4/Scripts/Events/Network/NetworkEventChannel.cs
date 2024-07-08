387aef3f07abdfbeee44a6e1e58407683cd9c31c
Ryan Lembo-Ehms <rlemboehms@gmail.com>
2024-07-08 15:47:59 -0400

    Create NetworkEventChannel

---
+++ Assets/LVL4/Scripts/Events/Network/NetworkEventChannel.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Network Event Channel", menuName = "Events/Network Event Channel")]
+public class NetworkEventChannel : GenericEventChannelSO { }
